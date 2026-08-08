68fed0f736d476d61af6848c4dc56b5678bcaa66
Yishen1011 <yishen1011@gmail.com>
2026-08-08 22:28:15 +0800

    feed follow and following

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -10,10 +10,10 @@ import (
 	"github.com/google/uuid"
 )
 
-func handlerAddFeed(s *state, cmd command) error {
+func handlerFollow(s *state, cmd command) error {
 
-	if len(cmd.Args) < 2 {
-		return errors.New("You must include 2 arguments\n")
+	if len(cmd.Args) < 1 {
+		return errors.New("The argument is empty\n")
 	}
 
 	// Get current login in user
@@ -22,21 +22,13 @@ func handlerAddFeed(s *state, cmd command) error {
 		return err
 	}
 
-	feedParams := database.CreateFeedParams{
-		ID:         uuid.New(),
-		CreatedAt:  time.Now().UTC(),
-		UpdatedAt:  time.Now().UTC(),
-		Name:       cmd.Args[0],
-		Url:        cmd.Args[1], 
-		UserID:     user.ID,
-	}
-
-	feed, err := s.db.CreateFeed(context.Background(), feedParams)
+	// Get feed from url
+	feed, err := s.db.GetFeedForURL(context.Background(), cmd.Args[0])
 	if err != nil {
 		return err
 	}
 
-	followParams := database.CreateFeedFollowParams{
+	args := database.CreateFeedFollowParams{
 		ID:         uuid.New(),
 		CreatedAt:  time.Now().UTC(),
 		UpdatedAt:  time.Now().UTC(),
@@ -44,14 +36,13 @@ func handlerAddFeed(s *state, cmd command) error {
 		FeedID:     feed.ID,
 	}
 
-	follow, err := s.db.CreateFeedFollow(context.Background(), followParams)
+	follow, err := s.db.CreateFeedFollow(context.Background(), args)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Feed is successfully added!\nName: %s\nUrl: %s\n", feed.Name, feed.Url)
 	fmt.Printf("Feed Follow is successfully added!\n")
 	fmt.Printf("Feed: %s\nUser: %s\n", follow.FeedName, follow.UserName)
-
+	
 	return nil
 }
